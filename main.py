3ac695b0e8f359c22659423365473ce6d825c92d
Dominik Dzikas <dominikdzikas@Dominiks-MacBook-Air.local>
2023-10-01 20:10:52 +0200

    added new elements

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Write your code here
 class Monkee:
 
-    def __init__(self, game_name: str):
+    def __init__(self, game_name: str, ui: str, ingame_menu_str: str):
         self.game_name = game_name
+        self.ui = ui
+        self.ingame_menu_str = ingame_menu_str
         self.username = "guest"
         self.menu_options = ["[Play]", "[High] scores", "[Help]", "[Exit]"]
 
@@ -24,13 +26,35 @@ class Monkee:
         print(f"Greetings, commander {self.username}!\nAre you ready to begin?\n[Yes] [No] Return to Main[Menu]")
         while True:
             choice = self.read_command(["yes", "no", "menu"])
+
             if choice == "yes":
-                print("\nGreat, now let's go code some more ;)")
-                return False
+                while True:
+                    print(self.ui)
+                    player_move = self.read_command(["ex", "up", "save", "m"])
+                    if player_move == "m":
+                        is_bool = self.ingame_menu()
+                        if is_bool in [True, False]:
+                            return is_bool
+
+                    else:
+                        print("Coming SOON!")
+                        return False
             elif choice == "no":
                 print("\nHow about now.\nAre you ready to begin?\n\t[Yes] [No]")
             elif choice == "menu":
                 return True
+
+    def ingame_menu(self):
+        print(self.ingame_menu_str)
+        while True:
+            ingame_choice = self.read_command(["back", "main", "save", "exit"])
+            if ingame_choice == "back":
+                break
+            elif ingame_choice == "main":
+                return True
+            else:
+                print("Coming SOON!")
+                return False
 
     def high_scores(self):
         print("\nNo scores to display.\n\t[Back]")
@@ -48,14 +72,40 @@ class Monkee:
 
 def main():
     gamename = """+===================================================
-     ███╗░░░███╗░█████╗░███╗░░██╗██╗░░██╗███████╗███████╗
-     ████╗░████║██╔══██╗████╗░██║██║░██╔╝██╔════╝██╔════╝
-     ██╔████╔██║██║░░██║██╔██╗██║█████═╝░█████╗░░█████╗░░
-     ██║╚██╔╝██║██║░░██║██║╚████║██╔═██╗░██╔══╝░░██╔══╝░░
-     ██║░╚═╝░██║╚█████╔╝██║░╚███║██║░╚██╗███████╗███████╗
-     ╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚══╝╚═╝░░╚═╝╚══════╝╚══════╝
-    +==================================================="""
-    game = Monkee(gamename)
+                  ███╗░░░███╗░█████╗░███╗░░██╗██╗░░██╗███████╗███████╗
+                  ████╗░████║██╔══██╗████╗░██║██║░██╔╝██╔════╝██╔════╝
+                  ██╔████╔██║██║░░██║██╔██╗██║█████═╝░█████╗░░█████╗░░
+                  ██║╚██╔╝██║██║░░██║██║╚████║██╔═██╗░██╔══╝░░██╔══╝░░
+                  ██║░╚═╝░██║╚█████╔╝██║░╚███║██║░╚██╗███████╗███████╗
+                  ╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚══╝╚═╝░░╚═╝╚══════╝╚══════╝
+                  +==================================================="""
+    ui = """\n+================================================================================================+                     
+         ▓▓▓▓▓▓▓▓▓▓▓▓▓▓         |        ▓▓▓▓▓▓▓▓▓▓▓▓▓▓          |       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       |      ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓        |     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+     ▓▓▓▓░░░░░░▓▓░░░░░░▓▓▓▓     |    ▓▓▓▓░░░░░░▓▓░░░░░░▓▓▓▓      |   ▓▓▓▓░░░░░░▓▓░░░░░░▓▓▓▓
+ ░░░░▓▓░░░░░░░░░░░░░░░░░░▓▓░░░░ | ░░░░▓▓░░░░░░░░░░░░░░░░░░▓▓░░░░ | ░░░░▓▓░░░░░░░░░░░░░░░░░░▓▓░░░░
+ ░░░░▓▓░░  ██░░░░░░  ██░░▓▓░░░░ | ░░░░▓▓░░  ██░░░░░░  ██░░▓▓░░░░ | ░░░░▓▓░░  ██░░░░░░  ██░░▓▓░░░░
+   ░░▓▓░░████░░░░░░████░░▓▓░░   |   ░░▓▓░░████░░░░░░████░░▓▓░░   |   ░░▓▓░░████░░░░░░████░░▓▓░░
+     ▓▓░░▒▒▒▒░░░░░░▒▒▒▒░░▓▓     |     ▓▓░░▒▒▒▒░░░░░░▒▒▒▒░░▓▓     |     ▓▓░░▒▒▒▒░░░░░░▒▒▒▒░░▓▓
+       ▓▓░░░░░░░░░░░░░░▓▓       |       ▓▓░░░░░░░░░░░░░░▓▓       |       ▓▓░░░░░░░░░░░░░░▓▓
+         ▓▓▓▓░░░░░░▓▓▓▓         |         ▓▓▓▓░░░░░░▓▓▓▓         |         ▓▓▓▓░░░░░░▓▓▓▓
+             ▓▓▓▓▓▓        ░░   |             ▓▓▓▓▓▓        ░░   |             ▓▓▓▓▓▓        ░░
+           ▓▓▓▓▓▓▓▓▓▓      ▓▓   |           ▓▓▓▓▓▓▓▓▓▓      ▓▓   |           ▓▓▓▓▓▓▓▓▓▓      ▓▓
+           ▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓   |           ▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓   |           ▓▓▓▓▓▓▓▓▓▓    ▓▓▓▓
+       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       |       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓       |       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
++================================================================================================+
+|                  [Ex]plore                          [Up]grade                                  |
+|                  [Save]                             [M]enu                                     |
++================================================================================================+"""
+    menu = """\n|==========================|
+              |            MENU          |
+              |                          |
+              | [Back] to game           |
+              | Return to [Main] Menu    |
+              | [Save] and exit          |
+              | [Exit] game              |
+              |==========================|"""
+    game = Monkee(gamename, ui, menu)
     game_status = True
     cases = {
              "play": game.play,
